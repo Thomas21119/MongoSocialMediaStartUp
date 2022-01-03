6c183859f2e72b47feaae8e6e3e4d7b0f9974eeb
Thomas21119 <thomas.wittwer2018@gmail.com>
2022-01-04 10:41:49 +1100

    initial commit done User schema

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: 'What do we call you',
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: 'Please enter a valid email address',
+      validate: {
+        validator(validEmail) {
+          return /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z]{2,6})(\.[a-z]{2,6})?$/.test(
+            validEmail
+          );
+        },
+        message: 'Please enter a valid email address',
+      },
+    },
+    thoughts: {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+    friends: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
