604446a879d370370a2fa430f402befc4ebb1ceb
Thomas21119 <thomas.wittwer2018@gmail.com>
2022-01-04 14:33:52 +1100

    up to date

--- server.js
+++ server.js
@@ -3,18 +3,24 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/socialMedia',
+  function (err, db) {
+    if (err) {
+      throw err;
+    }
+    console.log('db connected');
+    db.close();
+  }
+);
+
+mongoose.set('debug', true);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
-
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/socialMedia',
-  {}
-);
-
-mongoose.set('debug', true);
 
 app.listen(PORT, () => {
   console.log(`API server running on localhost:${PORT}!`);
