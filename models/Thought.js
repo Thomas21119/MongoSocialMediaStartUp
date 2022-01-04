3d8b82fc40376639ce5e375ec0ddfa754ff65411
Thomas21119 <thomas.wittwer2018@gmail.com>
2022-01-04 12:04:28 +1100

    index.js finish and cleaning thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,70 @@
-const User = require('./User');
-const Thought = require('./Thought');
+const { Schema, model, Types } = require('mongoose');
 
-module.exports = { User, Thought };
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: 'Please enter a valid reaction',
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: 'Please enter a valid username',
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      trim: true,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'Please enter a thought between 1 and 280 characters',
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    thoughtId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
